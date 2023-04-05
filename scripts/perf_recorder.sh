a7bc0990599ba1a26f5ef71850e0586851a99678
Antoine H <antoinehoe@gmail.com>
2023-04-05 17:35:41 +0200

    adding perf script to store cpu and ram usage to csv

--- scripts/perf_recorder.sh
+++ scripts/perf_recorder.sh
@@ -6,11 +6,11 @@ output_file=$HOME/BirdNET-Pi/perf_logs/perf.csv
 
 # Create the output file with column headers if it doesn't exist
 if [ ! -e "$output_file" ]; then
-  echo -e "---Timestamp---\tCPU (%)\tMem used (MiB)\tMem free (MiB)\tSwap used (MiB)\tSwap free (MiB)" >> "$output_file"
+  echo -e "\tTimestamp\tCPU \tMem used \tMem free \tSwap used \tSwap free " >> "$output_file"
 fi
 
 # Get current date and time
-timestamp=$(date +"%d-%m-%y %H:%M:%S")
+timestamp=$(date +"%d-%m %H:%M:%S")
 
 # Use top to get CPU and memory usage
 top_output=$(top -bn1 | grep "Cpu\|MiB Mem\|MiB Swap")
@@ -23,6 +23,4 @@ swap_used=$(echo "$top_output" | awk '{print $16}')
 swap_free=$(echo "$top_output" | awk '{print $18}')
 
 # Write data to the output file
-echo -e "$timestamp,\t$cpu,\t$mem_used,\t$mem_free,\t$swap_used,\t$swap_free" >> "$output_file"
-
-# Wait for 1 second before collecting the next data
+echo -e "$timestamp,\t$cpu(%),\t$mem_used(MiB),\t$mem_free(MiB),\t$swap_used(MiB),\t$swap_free(MiB)" >> "$output_file"
