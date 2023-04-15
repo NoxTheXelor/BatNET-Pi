8facdbdade9ac21d04ee975fb21a03f544515fff
Antoine H <antoinehoe@gmail.com>
2023-04-15 15:00:58 +0200

    readable output perf_recorder csv

--- scripts/perf_recorder.sh
+++ scripts/perf_recorder.sh
@@ -7,24 +7,22 @@ output_file="/home/bird/BirdNET-Pi/perf_logs/perf.csv"
 # Create the output file with column headers if it doesn't exist
 if [ ! -f ${output_file} ]; then
   touch ${output_file} && chmod g+rw ${output_file}
-  echo -e "Timestamp,CPU(%),Mem_used(MiB),Mem_free(MiB),Swap_used(MiB),Swap_free(MiB)" > ${output_file}
+  echo -e "Timestamp,Memory_Total,Free,Used,Swap_Free,Swap_Used,Swap_Total,CPU,Disk\n" > ${output_file}
 fi
 while true; do
   # Get current date and time
   timestamp=$(date +"%d-%m %H:%M:%S")
 
   # Use top to get CPU and memory usage
-  top_output=$(top -bn1 | grep "Cpu\|MiB Mem\|MiB Swap")
-
-  # Extract relevant data
-  cpu=$(echo "$top_output" | awk '{print $2}')
-  mem_used=$(echo "$top_output" | awk '{print $6}')
-  mem_free=$(echo "$top_output" | awk '{print $8}')
-  swap_used=$(echo "$top_output" | awk '{print $16}')
-  swap_free=$(echo "$top_output" | awk '{print $18}')
-
-  # Write data to the output file
-  echo -e "$timestamp,$cpu,$mem_used,$mem_free,$swap_used,$swap_free" >> ${output_file}
+  MEMORY_TOTAL=$(free -m | awk 'NR==1{printf "%.2f MB,", $2 }')
+  MEMORY_USED=$(free -m | awk 'NR==2{printf "%.2f MB,", $3 }')
+  MEMORY_FREE=$(free -m | awk 'NR==2{printf "%.2f MB,", $4 }')
+  SWAP_TOTAL=$(free -m | awk 'NR==4{printf "%.2f MB,", $2 }')
+  SWAP_USED=$(free -m | awk 'NR==4{printf "%.2f MB,", $3 }')
+  SWAP_FREE=$(free -m | awk 'NR==4{printf "%.2f MB,", $4 }')
+  CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%,", $(NF-2)}')
+  DISK=$(df -h | awk '$NF=="/"{printf "%s\n", $5}')
+  echo "$timestamp,$MEMORY_TOTAL,$MEMORY_FREE,$MEMORY_USED,$SWAP_FREE,$SWAP_USED,$SWAP_TOTAL,$CPU,$DISK" >> ${output_file}
   sleep 0.25  #approx 2 record/sec
 
 done
