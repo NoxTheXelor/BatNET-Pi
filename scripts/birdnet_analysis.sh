8c1614ecdb78e77660e2b6af6dc9f337f72e3e11
Antoine H <antoinehoe@gmail.com>
2023-04-17 12:17:41 +0200

    adding doc to move_analyzed() method from birdnet_analysis.sh

--- scripts/birdnet_analysis.sh
+++ scripts/birdnet_analysis.sh
@@ -61,9 +61,12 @@ get_files() {
 # Takes one argument:
 #   - {DIRECTORY}
 move_analyzed() {
+  # daily result record wave files if both conditions are met
+  # 1) bat detected
+  # 2) identification conf above threshold
   for i in "${files[@]}";do
     if [ -f "${1}/${i}" ];then
-      if grep -q ${i} "${1}/daily_result.csv";then
+      if grep -q ${i} "${1}/daily_result.csv";then #if name of wav file is in daily result
         if [ ! -d "${1}-Analyzed" ];then
           mkdir -p "${1}-Analyzed" && echo "'Analyzed' directory created"
         fi
